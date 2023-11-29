e3e0cbe33dd26fe1e21db52b40e2f058fe1cc419
Ketsadaphone <ketsadaphoneOfficial@gmail.com>
2023-11-30 01:59:42 +0700

    add responsive page

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,16 @@ banner.addEventListener("click", () => {
   //   resfresh page
   location.reload();
 });
+
+// Mobile
+const loginBtnMobile = document.querySelector("#loginBtn-mobile");
+
+loginBtnMobile.addEventListener("click", () => {
+  location.href = "./pages/login-page-mobile.html";
+});
+
+const resgisterBtnMobile = document.querySelector("#registerBtn-mobile");
+
+resgisterBtnMobile.addEventListener("click", () => {
+  location.href = "./pages/register-step1.html";
+});
