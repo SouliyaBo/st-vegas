01c712c2db842bb4427397c81e18408ec96e9d89
tolexDev <killer8master@gmail.com>
2023-12-01 02:24:58 +0700

    history modal

--- scripts/main.js
+++ scripts/main.js
@@ -52,3 +52,69 @@ function showModal(modalId) {
         closeModal(modal.id);
     });
   });
+
+
+// history modal
+const historyTitleDeposit = document.getElementById('history-title-deposit');
+const historyTitleWithdraw = document.getElementById('history-title-withdraw');
+const historyTitleBonus = document.getElementById('history-title-bonus');
+historyTitleDeposit.style.display = 'block';
+historyTitleWithdraw.style.display = 'none';
+historyTitleBonus.style.display = 'none';
+
+const tabDeposit = document.getElementById('tab-deposit');
+const tabWithdraw = document.getElementById('tab-withdraw');
+const tabBonus = document.getElementById('tab-bonus');
+tabDeposit.classList.add('active');
+tabWithdraw.classList.remove('active');
+tabBonus.classList.remove('active');
+
+const historyContentDeposit = document.getElementById('history-content-deposit');
+const historyContentWithdraw = document.getElementById('history-content-withdraw');
+const historyContentBonus = document.getElementById('history-content-bonus');
+historyContentDeposit.style.display = 'block';
+historyContentWithdraw.style.display = 'none';
+historyContentBonus.style.display = 'none';
+
+tabDeposit.addEventListener('click', function () {
+  historyTitleDeposit.style.display = 'block';
+  historyTitleWithdraw.style.display = 'none';
+  historyTitleBonus.style.display = 'none';
+  
+  tabDeposit.classList.add('active');
+  tabWithdraw.classList.remove('active');
+  tabBonus.classList.remove('active');
+  
+  historyContentDeposit.style.display = 'block';
+  historyContentWithdraw.style.display = 'none';
+  historyContentBonus.style.display = 'none';
+})
+
+tabWithdraw.addEventListener('click', function () {
+  historyTitleDeposit.style.display = 'none';
+  historyTitleWithdraw.style.display = 'block';
+  historyTitleBonus.style.display = 'none';
+  
+  tabDeposit.classList.remove('active');
+  tabWithdraw.classList.add('active');
+  tabBonus.classList.remove('active');
+  
+  historyContentDeposit.style.display = 'none';
+  historyContentWithdraw.style.display = 'block';
+  historyContentBonus.style.display = 'none';
+})
+
+tabBonus.addEventListener('click', function () {
+  historyTitleDeposit.style.display = 'none';
+  historyTitleWithdraw.style.display = 'none';
+  historyTitleBonus.style.display = 'block';
+  
+  tabDeposit.classList.remove('active');
+  tabWithdraw.classList.remove('active');
+  tabBonus.classList.add('active');
+  
+  historyContentDeposit.style.display = 'none';
+  historyContentWithdraw.style.display = 'none';
+  historyContentBonus.style.display = 'block';
+})
+// history modal end
