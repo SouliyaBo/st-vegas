3b2a819c72e7675c55ab60fb707b7762a946169b
Lailaolabdev <lailaolabdev@gmail.com>
2023-12-13 23:10:33 +0700

    registration

--- scripts/index.js
+++ scripts/index.js
@@ -83,17 +83,17 @@ footerLoginBtn.addEventListener("click", () => {
 const footerSignupBtn = document.querySelector("#footer-signup-button");
 const stepItemOne = document.getElementById("step-item1");
 const stepItemTwo = document.getElementById("step-item2");
-const stepItemThree = document.getElementById("step-item3");
+// const stepItemThree = document.getElementById("step-item3");
 
 const formStepOne = document.getElementById("form-step-one");
 const formStepTwo = document.getElementById("form-step-two");
-const formStepThree = document.getElementById("form-step-three");
+// const formStepThree = document.getElementById("form-step-three");
 formStepOne.style.display = "block";
 formStepTwo.style.display = "none";
 formStepThree.style.display = "none";
 
 const goToStepTwo = document.getElementById("goto-step2");
-const goToStepThree = document.getElementById("goto-step3");
+// const goToStepThree = document.getElementById("goto-step3");
 
 footerSignupBtn.addEventListener("click", function () {
   stepItemOne.classList.add("active");
